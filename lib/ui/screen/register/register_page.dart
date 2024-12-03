477d3c4b62e98a410f17737dffef68247b177beb
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-03 12:06:10 +0700

    fix: removed unused import

--- lib/ui/screen/register/register_page.dart
+++ lib/ui/screen/register/register_page.dart
@@ -1,5 +1,4 @@
 import 'package:employee_monitoring_app/component/em_button.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
