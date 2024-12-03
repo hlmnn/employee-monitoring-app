02c46da01f128ab5389259f59cf5090bce96d0ce
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-03 11:24:18 +0700

    fix(login): add route to register

--- lib/ui/screen/login/login_page.dart
+++ lib/ui/screen/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:employee_monitoring_app/component/em_button.dart';
+import 'package:employee_monitoring_app/ui/screen/register/register_page.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -74,10 +76,10 @@ class _LoginPageState extends State<LoginPage> {
                         controller: _password,
                         validator: (value) {
                           if (value == null || value.isEmpty) {
-                            return 'Password tidak boleh kosong';
+                            return 'Kata Sandi tidak boleh kosong';
                           }
                           if ((value.isNotEmpty) && value.length < 8) {
-                            return 'Password setidaknya memiliki panjang 8 karakter';
+                            return 'Kata Sandi setidaknya memiliki panjang 8 karakter';
                           }
                           return null;
                         },
@@ -117,26 +119,30 @@ class _LoginPageState extends State<LoginPage> {
                     onPressed: (){},
                     text: "Masuk"
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Belum mempunyai akun?'),
-                    TextButton(
-                      onPressed: (){},
-                      style: ElevatedButton.styleFrom(
-                        overlayColor: Colors.grey,
-                        padding: const EdgeInsets.only(left: 5),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                      ),
-                      child: const Text('Daftar Sekarang Disini!',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          decoration: TextDecoration.underline,
-                          color: Colors.black,
-                        ),
-                      ),
-                    )
-                  ],
+                const SizedBox(height: 18),
+                RichText(
+                  text: TextSpan(
+                      text: 'Belum mempunyai akun?',
+                      style: const TextStyle(color: Colors.black, fontSize: 12),
+                      children: <TextSpan>[
+                        TextSpan(text: ' Daftar sekarang disini!',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                decoration: TextDecoration.underline,
+                                color: Colors.black,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const RegisterPage(title: 'Register'),
+                                  ),
+                                );
+                              }
+                        )
+                      ]
+                  ),
                 ),
               ],
             ),
