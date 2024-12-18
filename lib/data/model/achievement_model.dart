7dd06b4519bda0eefb45549279def47a262deddd
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 10:34:43 +0700

    feat(achievement): added achievement page and model

---
+++ lib/data/model/achievement_model.dart
@@ -0,0 +1,9 @@
+class AchievementModel {
+  String image;
+  String title;
+  String description;
+  int level;
+  int taskTotal;
+
+  AchievementModel(this.image,this.title, this.description, this.level, this.taskTotal);
+}
--- lib/ui/screen/setting/achievement_page.dart
+++ lib/ui/screen/setting/achievement_page.dart
@@ -1,3 +1,4 @@
+import 'package:employee_monitoring_app/data/model/achievement_model.dart';
 import 'package:flutter/material.dart';
 
 class AchievementPage extends StatefulWidget {
@@ -10,6 +11,86 @@ class AchievementPage extends StatefulWidget {
 }
 
 class _AchievementPageState extends State<AchievementPage> {
+
+  List<AchievementModel> achievements = [
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Menyelesaikan Tugas Pertama Kali',
+      'Anda telah menyelesaikan tugas untuk pertama kalinya!',
+      100,
+      1,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Selesaikan Tugas 3 Kali',
+      'Anda telah menyelesaikan tugas sebanyak 3 kali!',
+      100,
+      3,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Selesaikan Tugas 5 Kali',
+      'Anda telah menyelesaikan tugas sebanyak 5 kali!',
+      100,
+      5,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Selesaikan Tugas 10 Kali',
+      'Anda telah menyelesaikan tugas sebanyak 10 kali!',
+      100,
+      10,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Selesaikan Tugas 25 Kali',
+      'Anda telah menyelesaikan tugas sebanyak 25 kali!',
+      100,
+      25,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Capai Level Berikutnya Pertama Kali',
+      'Anda telah mencapai level berikutnya untuk pertama kalinya!',
+      2,
+      100,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Capai Level 3',
+      'Anda telah mencapai level 3 pertama kalinya!',
+      3,
+      100,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Capai Level 5',
+      'Anda telah mencapai level 5 pertama kalinya!',
+      5,
+      100,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Capai Level 10',
+      'Anda telah mencapai level 10 pertama kalinya!',
+      10,
+      100,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Capai Level 25',
+      'Anda telah mencapai level 25 pertama kalinya!',
+      25,
+      100,
+    ),
+    AchievementModel(
+      'assets/images/app_logo.png',
+      'Capai Level 50',
+      'Anda telah mencapai level 50 pertama kalinya!',
+      50,
+      100,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,14 +117,67 @@ class _AchievementPageState extends State<AchievementPage> {
           ),
         ),
       ),
-      body: const SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Text('Achievements Page'),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: achievements.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == achievements.length-1) {
+                return ListTile(
+                  leading: Image.asset(achievements[index].image, width: 50),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(achievements[index].title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(achievements[index].description,
+                        style: const TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  child: ListTile(
+                    leading: Image.asset(achievements[index].image, width: 50),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(achievements[index].title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(achievements[index].description,
+                          style: const TextStyle(
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+            }
+          ),
         ),
-      ),
+      )
     );
   }
 }
