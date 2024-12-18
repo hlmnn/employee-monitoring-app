236a0485805c19ad96305bb08f444d94f80ee6cc
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 14:21:45 +0700

    feat(active_task_list): added card widget and add temporary task list

--- lib/ui/screen/monitor/monitor_home_active_task.dart
+++ lib/ui/screen/monitor/monitor_home_active_task.dart
@@ -1,5 +1,8 @@
 import 'package:employee_monitoring_app/component/em_alert_dialog/em_create_server_dialog.dart';
+import 'package:employee_monitoring_app/component/em_card/em_card.dart';
 import 'package:employee_monitoring_app/component/em_error.dart';
+import 'package:employee_monitoring_app/data/model/task_model.dart';
+import 'package:employee_monitoring_app/ui/screen/monitor/monitor_member_list_page.dart';
 import 'package:flutter/material.dart';
 
 class MonitorHomeActiveTaskPage extends StatefulWidget {
@@ -12,7 +15,6 @@ class MonitorHomeActiveTaskPage extends StatefulWidget {
 }
 
 class _MonitorHomeActiveTaskPageState extends State<MonitorHomeActiveTaskPage> {
-  final List<String> tasks = [];
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +26,16 @@ class _MonitorHomeActiveTaskPageState extends State<MonitorHomeActiveTaskPage> {
                 scrollDirection: Axis.vertical,
                 itemCount: tasks.length,
                 itemBuilder: (BuildContext context, int index) {
-                  // return EmCardTask(
-                  //   onTap: () {},
-                  //   title: 'Judul Task Aktif yang ke-${index + 1}',
-                  //   image: 'assets/images/avatar_placeholder.png',
-                  //   name: 'John Doe Hidayat',
-                  //   date: '7 Januari 2024',
-                  //   cash: '150',
-                  //   experience: '50',
-                  // );
+                  return EmCard.task(
+                    onTap: (){},
+                    image: 'assets/images/avatar_placeholder.png',
+                    title: '${tasks[index].title} yang ke-${index + 1}',
+                    name: tasks[index].member.name,
+                    level: tasks[index].member.level,
+                    date: tasks[index].lastDate,
+                    cash: tasks[index].cash,
+                    experience: tasks[index].exp,
+                  );
                 },
               )
             : EmError(
@@ -50,3 +53,60 @@ class _MonitorHomeActiveTaskPageState extends State<MonitorHomeActiveTaskPage> {
     );
   }
 }
+
+List<TaskModel> tasks = [
+  TaskModel(
+    'Buat Laporan Hasil Rapat',
+    Member(11,'John Doe Hidayat', 'Member', '17'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '28 Desember 2024',
+    'active',
+  ),
+  TaskModel(
+    'Buat Laporan Hasil Rapat',
+    Member(4,'Zendaya', 'Member', '10'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '30 Desember 2024',
+    'complete',
+  ),
+  TaskModel(
+    'Buat Laporan Pertanggungjawaban',
+    Member(6,'Sigma', 'Member', '8'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '24 Desember 2024',
+    'complete',
+  ),
+  TaskModel(
+    'Buat Laporan Pertanggungjawaban',
+    Member(2,'David', 'Member', '12'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '22 Desember 2024',
+    'active',
+  ),
+  TaskModel(
+    'Buat Laporan Kegiatan',
+    Member(7,'Chad', 'Member', '15'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '31 Desember 2024',
+    'complete',
+  ),
+  TaskModel(
+    'Buat Laporan Kegiatan',
+    Member(5,'Broski', 'Member', '22'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '23 Desember 2024',
+    'active',
+  ),
+];
