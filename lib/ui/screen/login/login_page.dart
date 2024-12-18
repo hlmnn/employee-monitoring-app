9c4cee5464e894ad16ea07884f4c6d6137ebb145
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 10:35:35 +0700

    refactor(login): added SafeArea inside Scaffold body

--- lib/ui/screen/login/login_page.dart
+++ lib/ui/screen/login/login_page.dart
@@ -29,146 +29,148 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Image.asset(
-                    'assets/images/app_logo.png',
-                    height: 175,
-                    width: 175,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Image.asset(
+                      'assets/images/app_logo.png',
+                      height: 175,
+                      width: 175,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _email,
-                        validator: (value) {
-                          return (value == null || value.isEmpty)
-                              ? 'Email tidak boleh kosong!'
-                              : null;
-                        },
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff461902)),
-                          ),
-                          errorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red),
-                          ),
-                          contentPadding: const EdgeInsets.all(10),
-                          hintText: 'Email',
-                          hintStyle: TextStyle(
-                            fontSize: 15,
-                          ),
-                          prefixIcon: Icon(Icons.email),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        controller: _password,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Kata Sandi tidak boleh kosong';
-                          }
-                          if ((value.isNotEmpty) && value.length < 8) {
-                            return 'Kata Sandi setidaknya memiliki panjang 8 karakter';
-                          }
-                          return null;
-                        },
-                        obscureText: _isObscure,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color(0xff461902),
-                            ),
-                          ),
-                          errorBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red),
-                          ),
-                          contentPadding: const EdgeInsets.all(10),
-                          hintText: 'Kata Sandi',
-                          hintStyle: const TextStyle(
-                            fontSize: 15,
-                          ),
-                          prefixIcon: const Icon(Icons.lock),
-                          suffixIcon: IconButton(
-                            icon: Icon(_isObscure
-                                ? Icons.visibility_off
-                                : Icons.visibility),
-                            onPressed: () {
-                              setState(() {
-                                _isObscure = !_isObscure;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 30),
-                EmButton.elevated(
-                  onPressed: (){
-                    if (_formKey.currentState!.validate()) {
-                      Future.delayed(Duration.zero, () {
-                        Navigator.of(context).pushAndRemoveUntil(
-                            MaterialPageRoute(
-                              builder: (context) => const MonitorNavigation(title: 'Monitor Navigation'),
-                            ),
-                                (Route<dynamic> route) => false);
-                      });
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 4,
-                    backgroundColor: const Color(0xffFFBD20),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  ),
-                  text: "Masuk",
-                  textStyle: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                      color: Colors.black
-                  ),
-                ),
-                const SizedBox(height: 18),
-                RichText(
-                  text: TextSpan(
-                      text: 'Belum mempunyai akun?',
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
-                        fontFamily: 'Poppins',
-                      ),
+                  const SizedBox(height: 10),
+                  Form(
+                    key: _formKey,
+                    child: Column(
                       children: [
-                        TextSpan(text: ' Daftar sekarang disini!',
-                            style: const TextStyle(
-                                fontWeight: FontWeight.w600,
-                                decoration: TextDecoration.underline,
-                                color: Colors.black,
+                        TextFormField(
+                          controller: _email,
+                          validator: (value) {
+                            return (value == null || value.isEmpty)
+                                ? 'Email tidak boleh kosong!'
+                                : null;
+                          },
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff461902)),
                             ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        const RegisterPage(title: 'Register'),
-                                  ),
-                                );
-                              }
-                        )
-                      ]
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                            ),
+                            contentPadding: EdgeInsets.all(10),
+                            hintText: 'Email',
+                            hintStyle: TextStyle(
+                              fontSize: 15,
+                            ),
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: _password,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Kata Sandi tidak boleh kosong';
+                            }
+                            if ((value.isNotEmpty) && value.length < 8) {
+                              return 'Kata Sandi setidaknya memiliki panjang 8 karakter';
+                            }
+                            return null;
+                          },
+                          obscureText: _isObscure,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color(0xff461902),
+                              ),
+                            ),
+                            errorBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                            ),
+                            contentPadding: const EdgeInsets.all(10),
+                            hintText: 'Kata Sandi',
+                            hintStyle: const TextStyle(
+                              fontSize: 15,
+                            ),
+                            prefixIcon: const Icon(Icons.lock),
+                            suffixIcon: IconButton(
+                              icon: Icon(_isObscure
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  _isObscure = !_isObscure;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 30),
+                  EmButton.elevated(
+                    onPressed: (){
+                      if (_formKey.currentState!.validate()) {
+                        Future.delayed(Duration.zero, () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                builder: (context) => const MonitorNavigation(title: 'Monitor Navigation'),
+                              ),
+                                  (Route<dynamic> route) => false);
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 4,
+                      backgroundColor: const Color(0xffFFBD20),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    ),
+                    text: "Masuk",
+                    textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.black
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Belum mempunyai akun?',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontFamily: 'Poppins',
+                        ),
+                        children: [
+                          TextSpan(text: ' Daftar sekarang disini!',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.black,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegisterPage(title: 'Register'),
+                                    ),
+                                  );
+                                }
+                          )
+                        ]
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
