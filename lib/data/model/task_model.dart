db847fc7ff23cb3677c46c734e9cc7c338c42bfb
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 14:45:13 +0700

    refactor(task_model): change the task list to inside task model class

--- lib/data/model/task_model.dart
+++ lib/data/model/task_model.dart
@@ -11,3 +11,60 @@ class TaskModel {
 
   TaskModel(this.title, this.member, this.cash, this.exp, this.startDate, this.lastDate, this.status);
 }
+
+List<TaskModel> tasks = [
+  TaskModel(
+    'Buat Laporan Hasil Rapat',
+    Member(11,'John Doe Hidayat', 'Member', '17'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '28 Desember 2024',
+    'active',
+  ),
+  TaskModel(
+    'Buat Laporan Hasil Rapat',
+    Member(4,'Zendaya', 'Member', '10'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '30 Desember 2024',
+    'complete',
+  ),
+  TaskModel(
+    'Buat Laporan Pertanggungjawaban',
+    Member(6,'Sigma', 'Member', '8'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '24 Desember 2024',
+    'complete',
+  ),
+  TaskModel(
+    'Buat Laporan Pertanggungjawaban',
+    Member(2,'David', 'Member', '12'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '22 Desember 2024',
+    'active',
+  ),
+  TaskModel(
+    'Buat Laporan Kegiatan',
+    Member(7,'Chad', 'Member', '15'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '31 Desember 2024',
+    'complete',
+  ),
+  TaskModel(
+    'Buat Laporan Kegiatan',
+    Member(5,'Broski', 'Member', '22'),
+    '150',
+    '50',
+    '20 Desember 2024',
+    '23 Desember 2024',
+    'active',
+  ),
+];
