f271da70f75a6f580f8f4ef4d738d822d4d3891b
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 10:33:00 +0700

    refactor(edit_profile): removed bottomNavBar with Stack to places the button on the bottom of the screen

--- lib/ui/screen/setting/edit_profile_page.dart
+++ lib/ui/screen/setting/edit_profile_page.dart
@@ -54,134 +54,143 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Stack(
-                alignment: Alignment.bottomRight,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
                 children: [
-                  const CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/avatar_placeholder.png'),
-                    radius: 90,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: (){},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffFFBD20),
+                  Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      const CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/avatar_placeholder.png'),
+                        radius: 90,
                       ),
-                      child: const Icon(Icons.edit, color: Colors.black, size: 20,),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: (){},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xffFFBD20),
+                          ),
+                          child: const Icon(Icons.edit, color: Colors.black, size: 20,),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _name,
+                    validator: (value) {
+                      return (value == null || value.isEmpty)
+                          ? 'Mohon isi Nama Lengkap anda!'
+                          : null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff461902)),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      contentPadding: const EdgeInsets.all(10),
+                      hintText: 'Nama Lengkap',
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                      ),
+                      prefixIcon: Icon(Icons.person),
                     ),
                   ),
-
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _phone,
+                    validator: (value) {
+                      return (value == null || value.isEmpty)
+                          ? 'Mohon isi No. HP anda!'
+                          : null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff461902)),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      contentPadding: const EdgeInsets.all(10),
+                      hintText: 'No. HP',
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                      ),
+                      prefixIcon: Icon(Icons.phone_android),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _address,
+                    validator: (value) {
+                      return (value == null || value.isEmpty)
+                          ? 'Mohon isi Alamat Lengkap anda!'
+                          : null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xff461902)),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      contentPadding: const EdgeInsets.all(10),
+                      hintText: 'Alamat',
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                      ),
+                      prefixIcon: Icon(Icons.home),
+                    ),
+                  ),
+                  const SizedBox(height: 100), // spacer
                 ],
               ),
-
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: _name,
-                validator: (value) {
-                  return (value == null || value.isEmpty)
-                      ? 'Mohon isi Nama Lengkap anda!'
-                      : null;
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff461902)),
+            )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Theme(
+              data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: EmButton.elevated(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Flushbar(
+                        message: 'Profile berhasil diubah!',
+                        flushbarPosition: FlushbarPosition.BOTTOM,
+                        margin: const EdgeInsets.all(8),
+                        borderRadius: BorderRadius.circular(10),
+                        duration: const Duration(seconds: 3),
+                        isDismissible: false,
+                      ).show(context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 4,
+                    backgroundColor: const Color(0xffFFBD20),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
+                  text: 'Konfirmasi',
+                  textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Colors.black
                   ),
-                  contentPadding: const EdgeInsets.all(10),
-                  hintText: 'Nama Lengkap',
-                  hintStyle: TextStyle(
-                    fontSize: 15,
-                  ),
-                  prefixIcon: Icon(Icons.person),
                 ),
               ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _phone,
-                validator: (value) {
-                  return (value == null || value.isEmpty)
-                      ? 'Mohon isi No. HP anda!'
-                      : null;
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff461902)),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  contentPadding: const EdgeInsets.all(10),
-                  hintText: 'No. HP',
-                  hintStyle: TextStyle(
-                    fontSize: 15,
-                  ),
-                  prefixIcon: Icon(Icons.phone_android),
-                ),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _address,
-                validator: (value) {
-                  return (value == null || value.isEmpty)
-                      ? 'Mohon isi Alamat Lengkap anda!'
-                      : null;
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff461902)),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  contentPadding: const EdgeInsets.all(10),
-                  hintText: 'Alamat',
-                  hintStyle: TextStyle(
-                    fontSize: 15,
-                  ),
-                  prefixIcon: Icon(Icons.home),
-                ),
-              ),
-            ],
+            )
           ),
-        )
-      ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 10),
-        child: EmButton.elevated(
-          onPressed: () {
-            if (_formKey.currentState!.validate()) {
-              Flushbar(
-                message: 'Profile berhasil diubah!',
-                flushbarPosition: FlushbarPosition.BOTTOM,
-                margin: const EdgeInsets.all(8),
-                borderRadius: BorderRadius.circular(10),
-                duration: const Duration(seconds: 3),
-                isDismissible: false,
-              ).show(context);
-            }
-          },
-          style: ElevatedButton.styleFrom(
-            elevation: 4,
-            backgroundColor: const Color(0xffFFBD20),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          ),
-          text: 'Konfirmasi',
-          textStyle: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
-              color: Colors.black
-          ),
-        ),
+        ],
       ),
     );
   }
