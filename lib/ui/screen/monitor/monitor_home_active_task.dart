335c39dd7e3208cb2f6f63f68cd0441dc242d327
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 14:25:24 +0700

    feat(active_task): added route to detail task page

--- lib/ui/screen/monitor/monitor_home_active_task.dart
+++ lib/ui/screen/monitor/monitor_home_active_task.dart
@@ -3,6 +3,7 @@ import 'package:employee_monitoring_app/component/em_card/em_card.dart';
 import 'package:employee_monitoring_app/component/em_error.dart';
 import 'package:employee_monitoring_app/data/model/task_model.dart';
 import 'package:employee_monitoring_app/ui/screen/monitor/monitor_member_list_page.dart';
+import 'package:employee_monitoring_app/ui/screen/monitor/task/monitor_task_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class MonitorHomeActiveTaskPage extends StatefulWidget {
@@ -27,7 +28,14 @@ class _MonitorHomeActiveTaskPageState extends State<MonitorHomeActiveTaskPage> {
                 itemCount: tasks.length,
                 itemBuilder: (BuildContext context, int index) {
                   return EmCard.task(
-                    onTap: (){},
+                    onTap: (){
+                      Navigator.push(context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                              const MonitorTaskDetailPage(title: 'Detail Tugas')
+                          )
+                      );
+                    },
                     image: 'assets/images/avatar_placeholder.png',
                     title: '${tasks[index].title} yang ke-${index + 1}',
                     name: tasks[index].member.name,
