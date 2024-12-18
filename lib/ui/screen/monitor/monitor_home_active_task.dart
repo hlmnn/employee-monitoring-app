0022fc40f5bcabc76bcb0f36f1d8501abba956d4
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-18 14:44:37 +0700

    feat(active_task): added condition to check whether task is active or not

--- lib/ui/screen/monitor/monitor_home_active_task.dart
+++ lib/ui/screen/monitor/monitor_home_active_task.dart
@@ -2,7 +2,6 @@ import 'package:employee_monitoring_app/component/em_alert_dialog/em_create_serv
 import 'package:employee_monitoring_app/component/em_card/em_card.dart';
 import 'package:employee_monitoring_app/component/em_error.dart';
 import 'package:employee_monitoring_app/data/model/task_model.dart';
-import 'package:employee_monitoring_app/ui/screen/monitor/monitor_member_list_page.dart';
 import 'package:employee_monitoring_app/ui/screen/monitor/task/monitor_task_detail_page.dart';
 import 'package:flutter/material.dart';
 
@@ -22,99 +21,44 @@ class _MonitorHomeActiveTaskPageState extends State<MonitorHomeActiveTaskPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: tasks.isNotEmpty
+        child: tasks.any((task) => task.status == 'active')
             ? ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: tasks.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return EmCard.task(
-                    onTap: (){
-                      Navigator.push(context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                              const MonitorTaskDetailPage(title: 'Detail Tugas')
-                          )
-                      );
-                    },
-                    image: 'assets/images/avatar_placeholder.png',
-                    title: '${tasks[index].title} yang ke-${index + 1}',
-                    name: tasks[index].member.name,
-                    level: tasks[index].member.level,
-                    date: tasks[index].lastDate,
-                    cash: tasks[index].cash,
-                    experience: tasks[index].exp,
-                  );
-                },
-              )
-            : EmError(
-              onPressed: (){
-                EmCreateServerDialog.show(context,
-                  onConfirm: (){},
-                  title: 'Buat Server',
-                  content: 'Tulis nama server sesuai yang Anda inginkan',
+          scrollDirection: Axis.vertical,
+          itemCount: tasks.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (tasks[index].status != 'active') return const SizedBox.shrink();
+            return EmCard.task(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MonitorTaskDetailPage(title: 'Detail Tugas'),
+                  ),
                 );
               },
-              textAbove: 'Anda belum mempunyai server.',
-              textBelow: 'Buat server terlebih dahulu!',
-            ),
+              image: 'assets/images/avatar_placeholder.png',
+              title: '${tasks[index].title} yang ke-${index + 1}',
+              name: tasks[index].member.name,
+              level: tasks[index].member.level,
+              date: tasks[index].lastDate,
+              cash: tasks[index].cash,
+              experience: tasks[index].exp,
+            );
+          },
+        )
+            : EmError(
+          onPressed: () {
+            EmCreateServerDialog.show(
+              context,
+              onConfirm: () {},
+              title: 'Buat Server',
+              content: 'Tulis nama server sesuai yang Anda inginkan',
+            );
+          },
+          textAbove: 'Anda belum mempunyai server.',
+          textBelow: 'Buat server terlebih dahulu!',
+        ),
       ),
     );
   }
 }
-
-List<TaskModel> tasks = [
-  TaskModel(
-    'Buat Laporan Hasil Rapat',
-    Member(11,'John Doe Hidayat', 'Member', '17'),
-    '150',
-    '50',
-    '20 Desember 2024',
-    '28 Desember 2024',
-    'active',
-  ),
-  TaskModel(
-    'Buat Laporan Hasil Rapat',
-    Member(4,'Zendaya', 'Member', '10'),
-    '150',
-    '50',
-    '20 Desember 2024',
-    '30 Desember 2024',
-    'complete',
-  ),
-  TaskModel(
-    'Buat Laporan Pertanggungjawaban',
-    Member(6,'Sigma', 'Member', '8'),
-    '150',
-    '50',
-    '20 Desember 2024',
-    '24 Desember 2024',
-    'complete',
-  ),
-  TaskModel(
-    'Buat Laporan Pertanggungjawaban',
-    Member(2,'David', 'Member', '12'),
-    '150',
-    '50',
-    '20 Desember 2024',
-    '22 Desember 2024',
-    'active',
-  ),
-  TaskModel(
-    'Buat Laporan Kegiatan',
-    Member(7,'Chad', 'Member', '15'),
-    '150',
-    '50',
-    '20 Desember 2024',
-    '31 Desember 2024',
-    'complete',
-  ),
-  TaskModel(
-    'Buat Laporan Kegiatan',
-    Member(5,'Broski', 'Member', '22'),
-    '150',
-    '50',
-    '20 Desember 2024',
-    '23 Desember 2024',
-    'active',
-  ),
-];
