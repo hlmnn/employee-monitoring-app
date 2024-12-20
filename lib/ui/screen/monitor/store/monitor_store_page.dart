7217a7a051ae5fd887f451010c9411947ebb9060
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-20 13:00:05 +0700

    feat(store): added gridview to show card reward list and route to create reward page

--- lib/ui/screen/monitor/store/monitor_store_page.dart
+++ lib/ui/screen/monitor/store/monitor_store_page.dart
@@ -1,3 +1,5 @@
+import 'package:employee_monitoring_app/component/em_card/em_card.dart';
+import 'package:employee_monitoring_app/ui/screen/monitor/store/monitor_reward_create_page.dart';
 import 'package:flutter/material.dart';
 
 class MonitorStorePage extends StatefulWidget {
@@ -14,13 +16,60 @@ class _MonitorStorePageState extends State<MonitorStorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xffFFBD20),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(56),
+        child: Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: AppBar(
+            title: Text(widget.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            backgroundColor: const Color(0xffFFBD20),
+          ),
+        ),
       ),
-      body: const Text('Store'),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200, // Maximum width of each item
+              crossAxisSpacing: 2, // Spacing between items
+              mainAxisSpacing: 2, // Vertical spacing between items
+              childAspectRatio: 3 / 4
+            ),
+            itemCount: 11,
+            itemBuilder: (BuildContext context, int index) {
+            return EmCard.item(
+              onTap: () {},
+              image: 'assets/images/avatar_placeholder.png',
+              name: 'Item Reward Name ke-${index+1}',
+              price: 100,
+              isMonitor: true,
+              onPressedButton: (){},
+            );
+          }
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: const Color(0xffFFBD20),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(context,
+            MaterialPageRoute(
+              builder: (context) =>
+              const MonitorRewardCreatePage(title: 'Buat Reward'),
+            ),
+          );
+        },
         child: const Icon(
           Icons.add,
           size: 30,
