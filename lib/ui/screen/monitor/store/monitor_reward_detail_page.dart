4639b87723517415545f6b380420f46582dbfe0b
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-23 11:53:11 +0700

    refactor(reward_detail): added detail reward page

---
+++ lib/ui/screen/monitor/store/monitor_reward_detail_page.dart
@@ -0,0 +1,154 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:employee_monitoring_app/component/em_button/em_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MonitorRewardDetailPage extends StatefulWidget {
+  const MonitorRewardDetailPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  _MonitorRewardDetailPageState createState() => _MonitorRewardDetailPageState();
+}
+
+class _MonitorRewardDetailPageState extends State<MonitorRewardDetailPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(56),
+        child: Container(
+          decoration: const BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: AppBar(
+            title: Text(widget.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            backgroundColor: const Color(0xffFFBD20),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/avatar_placeholder.png',
+                  width: double.infinity,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Expanded(
+                            child: Text('Nama Reward skdjosdk sjsdjkds idsjids sdij',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 19
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Image.asset('assets/images/cash.png', width: 20),
+                              const SizedBox(width: 3),
+                              const Text('100',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const Text('Deskripsi Reward',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in purus metus. Nullam magna arcu, convallis ac aliquam id, congue a sem. In non egestas mi. Sed sed sem sem. Donec et massa nunc."),
+                      const SizedBox(height: 150),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Theme(
+              data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 165,
+                      child: EmButton.elevated(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          elevation: 4,
+                          backgroundColor: const Color(0xffDD0000),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        ),
+                        text: 'Hapus',
+                        textStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                            color: Colors.black
+                        ),
+                        icon: const Icon(Icons.delete, color: Colors.black),
+                      ),
+                    ),
+                    const SizedBox(width: 9),
+                    SizedBox(
+                      width: 165,
+                      child: EmButton.elevated(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          elevation: 4,
+                          backgroundColor: const Color(0xffFFBD20),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        ),
+                        text: 'Edit',
+                        textStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                            color: Colors.black
+                        ),
+                        icon: const Icon(Icons.edit, color: Colors.black),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+
+}
