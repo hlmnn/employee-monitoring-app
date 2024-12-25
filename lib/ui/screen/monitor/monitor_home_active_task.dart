71582c1697578b585813ac1a5e6b1a4955ff82f8
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-25 23:43:43 +0700

    refactor(task_active): added condition when user is joined a server

--- lib/ui/screen/monitor/monitor_home_active_task.dart
+++ lib/ui/screen/monitor/monitor_home_active_task.dart
@@ -15,51 +15,54 @@ class MonitorHomeActiveTaskPage extends StatefulWidget {
 }
 
 class _MonitorHomeActiveTaskPageState extends State<MonitorHomeActiveTaskPage> {
+  final bool? isJoinServer = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: tasks.any((task) => task.status == 'active')
-          ? ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: tasks.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (tasks[index].status != 'active') return const SizedBox.shrink();
-              return EmCard.task(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const MonitorTaskDetailPage(title: 'Detail Tugas'),
-                    ),
-                  );
-                },
-                image: 'assets/images/avatar_placeholder.png',
-                title: '${tasks[index].title} yang ke-${index + 1}',
-                name: tasks[index].member.name,
-                level: tasks[index].member.level,
-                date: tasks[index].lastDate,
-                cash: tasks[index].cash,
-                experience: tasks[index].exp,
-              );
-            },
-          )
-          : EmError(
-            onPressed: () {
-              EmCreateServerDialog.show(
-                context,
-                onConfirm: () {},
-                title: 'Buat Server',
-                content: 'Tulis nama server sesuai yang Anda inginkan',
-              );
-            },
-            textAbove: 'Anda belum mempunyai server.',
-            textBelow: 'Buat server terlebih dahulu!',
-            isButton: true,
-          ),
-      ),
+    return isJoinServer == true
+      ? tasks.any((task) => task.status == 'active')
+        ? ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          scrollDirection: Axis.vertical,
+          itemCount: tasks.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (tasks[index].status != 'active') return const SizedBox.shrink();
+            return EmCard.task(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MonitorTaskDetailPage(title: 'Detail Tugas'),
+                  ),
+                );
+              },
+              image: 'assets/images/avatar_placeholder.png',
+              title: '${tasks[index].title} yang ke-${index + 1}',
+              name: tasks[index].member.name,
+              level: tasks[index].member.level,
+              date: tasks[index].lastDate,
+              cash: tasks[index].cash,
+              experience: tasks[index].exp,
+            );
+          },
+        )
+        : EmError(
+          onPressed: () {},
+          textAbove: 'Anda belum mempunyai tugas.',
+          textBelow: 'Buat tugas terlebih dahulu!',
+        )
+    : EmError(
+      onPressed: () {
+        EmCreateServerDialog.show(
+          context,
+          onConfirm: () {},
+          title: 'Buat Server',
+          content: 'Tulis nama server sesuai yang Anda inginkan',
+        );
+      },
+      textAbove: 'Anda belum mempunyai server.',
+      textBelow: 'Buat server terlebih dahulu!',
+      isButton: true,
     );
   }
 }
