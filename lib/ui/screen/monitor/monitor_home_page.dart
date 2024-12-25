886d6b785a290c99bf701ad9957c642c086513ae
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-25 23:49:35 +0700

    refactor(home): added nestedscrollview and safearea into tabbarview, changed appbar to sliverappbar,

--- lib/ui/screen/monitor/monitor_home_page.dart
+++ lib/ui/screen/monitor/monitor_home_page.dart
@@ -22,147 +22,89 @@ class _MonitorHomePageState extends State<MonitorHomePage> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(56),
-          child: Container(
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 5,
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget> [
+              SliverAppBar(
+                pinned: true,
+                floating: true,
+                forceElevated: innerBoxIsScrolled,
+                title: Text(widget.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
                 ),
-              ],
-            ),
-            child: AppBar(
-              title: Text(widget.title,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+                backgroundColor: const Color(0xffFFBD20),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.group, color: Colors.black),
+                    tooltip: 'Detail Group',
+                    onPressed: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                          const MonitorMemberListPage(title: 'Detail Group'),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  IconButton(
+                    icon: const Icon(Icons.leaderboard, color: Colors.black),
+                    tooltip: 'Peringkat',
+                    onPressed: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                          const LeaderboardPage(title: 'Peringkat'),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(width: 20),
+                ],
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(56),
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 20, right: 20, bottom: 5),
+                    child: TabBar(
+                      indicatorSize: TabBarIndicatorSize.tab,
+                      dividerColor: Colors.transparent,
+                      labelColor: const Color(0xffDD7402),
+                      labelStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                      unselectedLabelColor:Colors.black38,
+                      indicator: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10), // Creates border
+                        color: const Color(0xffFFE588),
+                      ),
+                      tabs: const [
+                        Tab(
+                          text: 'Task Aktif',
+                        ),
+                        Tab(
+                          text: 'Task Selesai',
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-              backgroundColor: const Color(0xffFFBD20),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.group, color: Colors.black),
-                  tooltip: 'Daftar Member',
-                  onPressed: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                        const MonitorMemberListPage(title: 'Daftar Member'),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(width: 10),
-                IconButton(
-                  icon: const Icon(Icons.leaderboard, color: Colors.black),
-                  tooltip: 'Peringkat Progress',
-                  onPressed: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                        const LeaderboardPage(title: 'Peringkat Progress'),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(width: 20),
+            ];
+          },
+          body: const SafeArea(
+            top: false,
+            bottom: false,
+            child: TabBarView(
+              children: [
+                MonitorHomeActiveTaskPage(title: 'Active Task'),
+                MonitorHomeCompletedTaskPage(title: 'Task Selesai')
               ],
             ),
           ),
-        ),
-        body: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: const Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Nama Group'),
-                            Text('PDIP Demokrat Golkar Gerindra PKB PPPPP',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 17
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Kode Invite Group'),
-                          Text('DDXFSQ6F',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text('Member'),
-                          Text('10',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 5),
-              child: TabBar(
-                indicatorSize: TabBarIndicatorSize.tab,
-                dividerColor: Colors.transparent,
-                labelColor: const Color(0xffDD7402),
-                labelStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Poppins',
-                ),
-                unselectedLabelColor:Colors.black38,
-                indicator: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), // Creates border
-                  color: const Color(0xffFFE588),
-                ),
-                tabs: const [
-                  Tab(
-                    text: 'Task Aktif',
-                  ),
-                  Tab(
-                    text: 'Task Selesai',
-                  )
-                ],
-              ),
-            ),
-            const Expanded(
-              child: TabBarView(
-                children: [
-                  MonitorHomeActiveTaskPage(title: 'Active Task'),
-                  MonitorHomeCompletedTaskPage(title: 'Task Selesai')
-                ],
-              ),
-            ),
-          ],
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: const Color(0xffFFBD20),
