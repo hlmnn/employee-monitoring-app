34998fc996d7590a2aecc40aafaa15d50ad6d17f
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-25 23:44:02 +0700

    refactor(task_complete): added condition when user is joined a server

--- lib/ui/screen/monitor/monitor_home_completed task.dart
+++ lib/ui/screen/monitor/monitor_home_completed task.dart
@@ -1,5 +1,8 @@
 import 'package:employee_monitoring_app/component/em_alert_dialog/em_create_server_dialog.dart';
+import 'package:employee_monitoring_app/component/em_card/em_card.dart';
 import 'package:employee_monitoring_app/component/em_error.dart';
+import 'package:employee_monitoring_app/data/model/task_model.dart';
+import 'package:employee_monitoring_app/ui/screen/monitor/task/monitor_task_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class MonitorHomeCompletedTaskPage extends StatefulWidget {
@@ -12,42 +15,54 @@ class MonitorHomeCompletedTaskPage extends StatefulWidget {
 }
 
 class _MonitorHomeCompletedTaskPageState extends State<MonitorHomeCompletedTaskPage> {
-  final List<String> tasks = [];
+  final bool? isJoinServer = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: tasks.isNotEmpty
-              ? ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: tasks.length,
-            itemBuilder: (BuildContext context, int index) {
-              // return EmCardTask(
-              //   onTap: () {},
-              //   title: 'Judul Task Selesai yang ke-${index + 1}',
-              //   image: 'assets/images/avatar_placeholder.png',
-              //   name: 'John Doe Hidayat',
-              //   date: '7 Januari 2024',
-              //   cash: '150',
-              //   experience: '50',
-              // );
-            },
-          )
-              : EmError(
-                onPressed: (){
-                  EmCreateServerDialog.show(context,
-                    onConfirm: (){},
-                    title: 'Buat Server',
-                    content: 'Tulis nama server sesuai yang Anda inginkan',
-                  );
-                },
-                textAbove: 'Anda belum mempunyai server.',
-                textBelow: 'Buat server terlebih dahulu!',
-                isButton: true,
+    return isJoinServer == true
+        ? tasks.any((task) => task.status == 'complete')
+        ? ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      scrollDirection: Axis.vertical,
+      itemCount: tasks.length,
+      itemBuilder: (BuildContext context, int index) {
+        if (tasks[index].status != 'complete') return const SizedBox.shrink();
+        return EmCard.task(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const MonitorTaskDetailPage(title: 'Detail Tugas'),
               ),
-      ),
+            );
+          },
+          image: 'assets/images/avatar_placeholder.png',
+          title: '${tasks[index].title} yang ke-${index + 1}',
+          name: tasks[index].member.name,
+          level: tasks[index].member.level,
+          date: tasks[index].lastDate,
+          cash: tasks[index].cash,
+          experience: tasks[index].exp,
+        );
+      },
+    )
+        : EmError(
+      onPressed: () {},
+      textAbove: 'Anda belum mempunyai tugas.',
+      textBelow: 'Buat tugas terlebih dahulu!',
+    )
+        : EmError(
+      onPressed: () {
+        EmCreateServerDialog.show(
+          context,
+          onConfirm: () {},
+          title: 'Buat Server',
+          content: 'Tulis nama server sesuai yang Anda inginkan',
+        );
+      },
+      textAbove: 'Anda belum mempunyai server.',
+      textBelow: 'Buat server terlebih dahulu!',
+      isButton: true,
     );
   }
 }
