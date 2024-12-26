58dd9e246d4192b097c9899096515bc55bbc6171
Hilman Fauzi Herdiana <kk726627@gmail.com>
2024-12-26 10:20:09 +0700

    feat(reward_detail): added buttons to bottom of the screen

--- lib/ui/screen/monitor/task/monitor_task_detail_page.dart
+++ lib/ui/screen/monitor/task/monitor_task_detail_page.dart
@@ -1,4 +1,7 @@
+import 'package:employee_monitoring_app/component/em_alert_dialog.dart';
+import 'package:employee_monitoring_app/component/em_button/em_button.dart';
 import 'package:employee_monitoring_app/component/em_card/em_card.dart';
+import 'package:employee_monitoring_app/ui/screen/monitor/task/monitor_task_edit_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -37,121 +40,189 @@ class _MonitorTaskDetailPageState extends State<MonitorTaskDetailPage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Lorem ipsum dolor sit amet, consectetur elit. Curabitur in purus metus.',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
+                const Text('Lorem ipsum dolor sit amet, consectetur elit. Curabitur in purus metus.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text('Reward',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Row(
+                    Column(
                       children: [
-                        Image.asset('assets/images/cash.png', width: 14),
-                        const SizedBox(width: 3),
-                        const Text('50'),
+                        const Text('Reward',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Image.asset('assets/images/cash.png', width: 14),
+                            const SizedBox(width: 3),
+                            const Text('50'),
+                          ],
+                        )
                       ],
-                    )
+                    ),
+                    const Column(
+                      children: [
+                        Text('Deadline',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('28 Des 2024'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text('Status',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.green.shade100,
+                            border: Border.all(
+                              color: Colors.green.shade300, // Border color
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Text('Aktif',
+                            style: TextStyle(
+                              color: Colors.green
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ],
                 ),
-                const Column(
-                  children: [
-                    Text('Deadline',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text('28 Des 2024'),
-                  ],
+                const SizedBox(height: 10),
+                EmCard.member(
+                  onTap: (){},
+                  image: 'assets/images/avatar_placeholder.png',
+                  name: 'Hilman Fauzi Herdiana',
+                  role: 'Member',
+                  level: '10',
                 ),
-                Column(
-                  children: [
-                    const Text('Status',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                const SizedBox(height: 10),
+                const Text('Deskripsi Tugas',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in purus metus.'),
+                const SizedBox(height: 3),
+                const SizedBox(height: 10),
+                const Text('Laporan Hasil Tugas',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 3),
+                const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in purus metus. Nullam magna arcu, convallis ac aliquam id, congue a sem. In non egestas mi. Sed sed sem sem. Donec et massa nunc."),
+                const SizedBox(height: 10),
+                const Text('File Bukti',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 3),
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey,
                       ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 10, right: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.green.shade100,
-                        border: Border.all(
-                          color: Colors.green.shade300, // Border color
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const Text('Aktif',
-                        style: TextStyle(
-                          color: Colors.green
-                        ),
-                      ),
-                    )
-                  ],
+                  ),
+                  child: ListTile(
+                    onTap: () {},
+                    title: const Text('Laporan-Hasil-Rapat.pdf'),
+                    trailing: const Icon(Icons.download_rounded),
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 10),
-            EmCard.member(
-              onTap: (){},
-              image: 'assets/images/avatar_placeholder.png',
-              name: 'Hilman Fauzi Herdiana',
-              role: 'Member',
-              level: '10',
-            ),
-            const SizedBox(height: 10),
-            const Text('Deskripsi Tugas',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Theme(
+              data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+              child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 165,
+                        child: EmButton.elevated(
+                          onPressed: () {
+                            EmAlertDialog.show(context,
+                              onConfirm: (){},
+                              title: 'Peringatan!',
+                              content: 'Task yang dipilih akan Anda hapus. Apakah Anda yakin ingin menghapus task ini?',
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 4,
+                            backgroundColor: const Color(0xffDD0000),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                          ),
+                          text: 'Hapus',
+                          textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              color: Colors.black
+                          ),
+                          icon: const Icon(Icons.delete, color: Colors.black),
+                        ),
+                      ),
+                      const SizedBox(width: 9),
+                      SizedBox(
+                        width: 165,
+                        child: EmButton.elevated(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MonitorTaskEditPage(title: 'Edit Tugas'),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            elevation: 4,
+                            backgroundColor: const Color(0xffFFBD20),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                          ),
+                          text: 'Edit',
+                          textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              color: Colors.black
+                          ),
+                          icon: const Icon(Icons.edit, color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  )
               ),
             ),
-            const Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in purus metus.'),
-            const SizedBox(height: 3),
-            const SizedBox(height: 10),
-            const Text('Laporan Hasil Tugas',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 3),
-            const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in purus metus. Nullam magna arcu, convallis ac aliquam id, congue a sem. In non egestas mi. Sed sed sem sem. Donec et massa nunc."),
-            const SizedBox(height: 10),
-            const Text('File Bukti',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 3),
-            Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              child: ListTile(
-                onTap: () {},
-                title: const Text('Laporan-Hasil-Rapat.pdf'),
-                trailing: const Icon(Icons.download_rounded),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
