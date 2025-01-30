ed13d5e617e0283fc4c5265344fc6fbd54aae714
Hilman Fauzi Herdiana <kk726627@gmail.com>
2025-01-30 11:18:02 +0700

    feat: added AuthUserModel

--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -20,3 +20,44 @@ List<UserModel> members = [
   UserModel(10,'Ahmad', 'Member', '7'),
   UserModel(11,'John Doe Hidayat', 'Member', '17'),
 ];
+
+class AuthUserModel {
+  String id;
+  String name;
+  String phone;
+  String email;
+  bool isMonitor;
+  int level;
+  int currentExp;
+  int maxExp;
+  String address;
+  int serverId;
+
+  AuthUserModel({
+    required this.id,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.isMonitor,
+    required this.level,
+    required this.currentExp,
+    required this.maxExp,
+    required this.address,
+    required this.serverId,
+  });
+
+  factory AuthUserModel.fromJson(Map<String, dynamic> json) {
+    return AuthUserModel(
+      id: json['id'],
+      name: json['name'],
+      phone: json['phone'],
+      email: json['email'],
+      isMonitor: json['is_monitor'],
+      level: json['level'],
+      currentExp: json['current_exp'],
+      maxExp: json['max_exp'],
+      address: json['address'] ?? '',
+      serverId: json['server_id'] ?? 0,
+    );
+  }
+}
