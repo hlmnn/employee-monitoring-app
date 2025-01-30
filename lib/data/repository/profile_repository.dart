a7832cb8300dc53efc15ac465955524c9cce8ac8
Hilman Fauzi Herdiana <kk726627@gmail.com>
2025-01-30 11:19:46 +0700

    feat: added new function to get user profile and update profile

--- lib/data/repository/profile_repository.dart
+++ lib/data/repository/profile_repository.dart
@@ -1,15 +1,42 @@
+import 'package:employee_monitoring_app/data/model/user_model.dart';
 import 'package:employee_monitoring_app/main.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfileRepository {
   // logout user
   Future<bool> logout() async {
-    await supabase.auth.signOut();
-    return true;
+    try {
+      await supabase.auth.signOut();
+      return true;
+    } catch (e) {
+      rethrow;
+    }
   }
 
-  Future<User?> getCurrentUser() async {
-    final user = supabase.auth.currentUser;
-    return user;
+  Future<AuthUserModel> getCurrentUserProfile() async {
+    try {
+      final session = supabase.auth.currentSession;
+      final response = await supabase.from('profiles').select().eq('id', session!.user.id).single();
+      final data = AuthUserModel.fromJson(response);
+      return data;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<bool> updateUserProfile(name, phone, address) async {
+    try {
+      final session = supabase.auth.currentSession;
+      final updates = {
+        'id': session!.user.id,
+        'name': name,
+        'phone': phone,
+        // 'address': address,
+        // 'updated_at': DateTime.now().toIso8601String(),
+      };
+      await supabase.from('profiles').upsert(updates);
+      return true;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
--- lib/ui/cubit/profile_cubit.dart
+++ lib/ui/cubit/profile_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:employee_monitoring_app/data/data_state.dart';
+import 'package:employee_monitoring_app/data/model/user_model.dart';
 import 'package:employee_monitoring_app/data/repository/profile_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,7 +9,7 @@ class ProfileCubit extends Cubit<DataState>{
 
   ProfileCubit() : super(InitialState());
 
-  Future<void> logout() async {
+  void logout() async {
     try{
       emit(LoadingState());
       final data = await repository.logout();
@@ -21,13 +22,27 @@ class ProfileCubit extends Cubit<DataState>{
     }
   }
 
-  // get user on the current session
-  Future<void> getCurrentUser() async {
+  // get current user profile
+  void getCurrentUserProfile() async {
     try{
       emit(LoadingState());
-      final data = await repository.getCurrentUser();
-      emit(SuccessState<User?>(data));
-    } on AuthException catch (e) {
+      final data = await repository.getCurrentUserProfile();
+      emit(SuccessState<AuthUserModel>(data));
+    } on PostgrestException catch (e) {
+      emit(ErrorState(e.message));
+    } catch (e) {
+      emit(ErrorState(e.toString()));
+      rethrow;
+    }
+  }
+
+  // update user profile
+  void updateUserProfile(name, phone, address) async {
+    try{
+      emit(LoadingState());
+      final data = await repository.updateUserProfile(name, phone, address);
+      emit(SuccessState<bool?>(data));
+    } on PostgrestException catch (e) {
       emit(ErrorState(e.message));
     } catch (e) {
       emit(ErrorState(e.toString()));
