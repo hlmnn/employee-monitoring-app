1e7d483b5d6d6ff6d8f9767936fbf3df7d45cf92
Hilman Fauzi Herdiana <kk726627@gmail.com>
2025-02-04 10:28:19 +0700

    feat: added getLeaderboard to show users task progress

--- lib/ui/screen/leaderboard/leaderboard_page.dart
+++ lib/ui/screen/leaderboard/leaderboard_page.dart
@@ -1,8 +1,12 @@
 import 'package:employee_monitoring_app/component/em_card/em_card.dart';
+import 'package:employee_monitoring_app/component/em_circular_loading.dart';
 import 'package:employee_monitoring_app/component/em_error.dart';
+import 'package:employee_monitoring_app/data/data_state.dart';
 import 'package:employee_monitoring_app/data/model/user_model.dart';
+import 'package:employee_monitoring_app/ui/cubit/leaderboard_cubit.dart';
 import 'package:employee_monitoring_app/ui/screen/monitor/monitor_member_detail_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LeaderboardPage extends StatefulWidget {
   const LeaderboardPage({super.key, required this.title});
@@ -17,6 +21,7 @@ class _LeaderboardPageState extends State<LeaderboardPage>{
 
   @override
   Widget build(BuildContext context) {
+    context.read<LeaderboardCubit>().getLeaderboard();
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(56),
@@ -40,53 +45,66 @@ class _LeaderboardPageState extends State<LeaderboardPage>{
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 10, bottom: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Peringkat'),
-                  Text('Task Selesai'),
-                ],
+      body: BlocBuilder<LeaderboardCubit, DataState>(
+        builder: (context, state) {
+          List<AuthUserModel> members = [];
+          if (state is LoadingState) {
+            return const Center(
+                child: EmCircularLoading(size: 30, color: Color(0xffFFBD20))
+            );
+          } else if (state is SuccessState) {
+            members = state.data;
+          }
+
+          if (members.isEmpty) {
+            return EmError(
+              onPressed: () {},
+              textAbove: 'Peringkat masih kosong.',
+              textBelow: 'Tambah member terlebih dahulu!',
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Peringkat'),
+                      Text('Task Selesai'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: members.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return EmCard.leaderboard(
+                        onTap: () {
+                          Navigator.push(context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                              const MonitorMemberDetailPage(title: 'Detail Member'),
+                            ),
+                          );
+                        },
+                        rank: '${index + 1}',
+                        image: 'assets/images/avatar_placeholder.png',
+                        name: members[index].name,
+                        level: '${members[index].level}',
+                        taskTotal: '${members[index].taskCompletedMonth}',
+                      );
+                    },
+                  )
               ),
-            ),
-            Expanded(
-              child: members.any((member) => member.role == 'Member')
-                ? ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: members.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    if (members[index].role != 'Member') return const SizedBox.shrink();
-                    return EmCard.leaderboard(
-                      onTap: () {
-                        Navigator.push(context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                            const MonitorMemberDetailPage(title: 'Detail Member'),
-                          ),
-                        );
-                      },
-                      rank: '${members.where((member) => member.role == 'Member').toList().indexOf(members[index]) + 1}',
-                      image: 'assets/images/avatar_placeholder.png',
-                      name: members[index].name,
-                      level: members[index].level,
-                      taskTotal: '${30 - (index * 2 + 5)}',
-                    );
-                  },
-                )
-              : EmError(
-                onPressed: (){},
-                textAbove: 'Peringkat progress masih kosong.',
-                textBelow: 'Tambah member terlebih dahulu!',
-              ),
-            ),
-          ],
-        )
-      ),
+            ],
+          )
+        );
+      }),
     );
   }
 }
