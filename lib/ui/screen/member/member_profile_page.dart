0edf91efe987130c8af0c335e9746b40c4b7ee85
Hilman Fauzi Herdiana <kk726627@gmail.com>
2025-02-04 10:52:50 +0700

    feat: added getCurrentUserProfile function to show current user profile

--- lib/ui/screen/member/member_profile_page.dart
+++ lib/ui/screen/member/member_profile_page.dart
@@ -1,9 +1,13 @@
+import 'package:employee_monitoring_app/choose_login_as_page.dart';
 import 'package:employee_monitoring_app/component/em_button/em_button.dart';
-import 'package:employee_monitoring_app/ui/screen/login/login_page.dart';
+import 'package:employee_monitoring_app/component/em_circular_loading.dart';
+import 'package:employee_monitoring_app/data/data_state.dart';
+import 'package:employee_monitoring_app/ui/cubit/profile_cubit.dart';
 import 'package:employee_monitoring_app/ui/screen/setting/achievement_page.dart';
 import 'package:employee_monitoring_app/ui/screen/setting/change_password_page.dart';
 import 'package:employee_monitoring_app/ui/screen/setting/edit_profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MemberProfilePage extends StatefulWidget {
   const MemberProfilePage({super.key, required this.title});
@@ -15,22 +19,9 @@ class MemberProfilePage extends StatefulWidget {
 }
 
 class _MemberProfilePageState extends State<MemberProfilePage> {
-  int level = 1;
-  int currentTotalExp = 7900; // example starting experience (level 7)
-  static const int baseMaxExp = 100;
-  int maxExp = baseMaxExp;
-
-  Future<void> calculateExp() async {
-    while (currentTotalExp >= maxExp) {
-      currentTotalExp -= maxExp; // subtract maxExp from currentExp
-      level++; // increase the level
-      maxExp = baseMaxExp + (level - 1) * 50; // recalculate maxExp
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    calculateExp();
+    context.read<ProfileCubit>().getCurrentUserProfile();
 
     return Scaffold(
       appBar: PreferredSize(
@@ -55,209 +46,237 @@ class _MemberProfilePageState extends State<MemberProfilePage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.only(left: 20, right: 20,top: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                const CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/avatar_placeholder.png'),
-                  radius: 40,
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Expanded(
-                            child: Text('John Doe Hidayat',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+      body: BlocBuilder<ProfileCubit, DataState>(
+        builder: (context, state) {
+          String name = '';
+          String role = '';
+          int cash = 0;
+          int level = 0;
+          int currentExp = 0;
+          int maxExp = 1;
+
+          if (state is LoadingState) {
+            return const Center(
+                child: EmCircularLoading(size: 30, color: Color(0xffFFBD20))
+            );
+          } else if (state is SuccessState) {
+            if (state.data is bool) {
+              context.read<ProfileCubit>().resetState();
+              Future.delayed(Duration.zero, () {
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                      const ChooseLoginAsPage(title: 'Choose Login Page'),
+                    ), (Route<dynamic> route) => false);
+              });
+            } else {
+              name = state.data.name;
+              cash = state.data.cash;
+              level = state.data.level;
+              currentExp = state.data.currentExp;
+              maxExp = state.data.maxExp;
+              state.data.isMonitor == true ? role = 'Monitor' : role = 'Member';
+            }
+          }
+
+          return SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.only(left: 20, right: 20,top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/avatar_placeholder.png'),
+                    radius: 40,
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Text(name,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                          RichText(
-                            text: TextSpan(
-                              text: 'Lv.',
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                                fontFamily: 'Poppins',
+                            RichText(
+                              text: TextSpan(
+                                text: 'Lv.',
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                  fontFamily: 'Poppins',
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: '$level',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
                               ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Column(
+                          children: [
+                            Row(
                               children: [
-                                TextSpan(
-                                  text: '$level',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
+                                Expanded(
+                                  child: SizedBox(
+                                    height: 15,
+                                    child: LinearProgressIndicator(
+                                      value: currentExp / maxExp,
+                                      backgroundColor: const Color(0xffffecc0),
+                                      color: const Color(0xffFFBD20),
+                                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                                    ),
                                   ),
                                 ),
                               ],
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 5),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: SizedBox(
-                                  height: 15,
-                                  child: LinearProgressIndicator(
-                                    value: currentTotalExp / maxExp,
-                                    backgroundColor: const Color(0xffffecc0),
-                                    color: const Color(0xffFFBD20),
-                                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('$currentExp/$maxExp',
+                                  style: const TextStyle(
+                                    fontSize: 11,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('$currentTotalExp/$maxExp',
-                                style: const TextStyle(
-                                  fontSize: 11,
+                                const Text('Experience',
+                                  style: TextStyle(
+                                    fontSize: 11,
+                                  ),
                                 ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(role,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
                               ),
-                              const Text('Experience',
-                                style: TextStyle(
-                                  fontSize: 11,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Member',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
                             ),
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Image.asset('assets/images/cash.png', height: 12),
-                              const SizedBox(width: 5),
-                              const Text('1500',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 17,
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image.asset('assets/images/cash.png', height: 12),
+                                const SizedBox(width: 5),
+                                Text('$cash',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Column(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      leading: const Icon(Icons.emoji_events, color: Colors.black),
+                      title: const Text('Achievements'),
+                      trailing: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black),
+                      onTap: () {
+                        Navigator.push(context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                            const AchievementPage(title: 'Achievements'),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Column(
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.grey,
-                      ),
+                        );
+                      },
                     ),
                   ),
-                  child: ListTile(
-                    leading: const Icon(Icons.emoji_events, color: Colors.black),
-                    title: const Text('Achievements'),
-                    trailing: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black),
-                    onTap: () {
-                      Navigator.push(context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                          const AchievementPage(title: 'Achievements'),
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
                         ),
-                      );
-                    },
-                  ),
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.grey,
                       ),
                     ),
+                    child: ListTile(
+                      leading: const Icon(Icons.edit_square, color: Colors.black),
+                      title: const Text('Ubah Profile'),
+                      trailing: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black),
+                      onTap: () {
+                        Navigator.push(context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                            const EditProfilePage(title: 'Ubah Profile',),
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                  child: ListTile(
+                  ListTile(
                     leading: const Icon(Icons.edit_square, color: Colors.black),
-                    title: const Text('Ubah Profile'),
+                    title: const Text('Ganti Kata Sandi'),
                     trailing: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black),
                     onTap: () {
                       Navigator.push(context,
                         MaterialPageRoute(
                           builder: (context) =>
-                          const EditProfilePage(title: 'Ubah Profile'),
+                          const ChangePasswordPage(title: 'Ganti Kata Sandi'),
                         ),
                       );
                     },
                   ),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.edit_square, color: Colors.black),
-                  title: const Text('Ganti Kata Sandi'),
-                  trailing: const Icon(Icons.arrow_forward_ios_rounded, color: Colors.black),
-                  onTap: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                        const ChangePasswordPage(title: 'Ganti Kata Sandi'),
-                      ),
-                    );
-                  },
-                ),
-                const SizedBox(height: 20),
-                EmButton.elevated(
-                  onPressed: (){
-                    Future.delayed(Duration.zero, () {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(title: 'Login'),
-                        )
-                      );
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 4,
-                    backgroundColor: const Color(0xffDD0000),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  const SizedBox(height: 20),
+                  EmButton.elevated(
+                    onPressed: (){
+                      context.read<ProfileCubit>().logout();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 4,
+                      backgroundColor: const Color(0xffDD0000),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    ),
+                    text: 'Logout',
+                    icon: const Icon(Icons.logout, color: Colors.black),
+                    textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.black
+                    ),
                   ),
-                  text: 'Logout',
-                  icon: const Icon(Icons.logout, color: Colors.black),
-                  textStyle: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                      color: Colors.black
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
