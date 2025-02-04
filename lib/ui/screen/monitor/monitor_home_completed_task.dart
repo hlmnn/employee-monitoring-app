636474b9d9ac750c7ca38b97ff4f44416045f890
Hilman Fauzi Herdiana <kk726627@gmail.com>
2025-02-04 21:08:18 +0700

    feat: added function to get list of active and completed tasks

--- lib/ui/screen/monitor/monitor_home_completed_task.dart
+++ lib/ui/screen/monitor/monitor_home_completed_task.dart
@@ -1,9 +1,14 @@
 import 'package:employee_monitoring_app/component/em_alert_dialog/em_create_group_dialog.dart';
 import 'package:employee_monitoring_app/component/em_card/em_card.dart';
+import 'package:employee_monitoring_app/component/em_circular_loading.dart';
 import 'package:employee_monitoring_app/component/em_error.dart';
+import 'package:employee_monitoring_app/data/data_state.dart';
 import 'package:employee_monitoring_app/data/model/task_model.dart';
+import 'package:employee_monitoring_app/ui/cubit/task_cubit.dart';
 import 'package:employee_monitoring_app/ui/screen/monitor/task/monitor_task_detail_page.dart';
+import 'package:employee_monitoring_app/utils/date_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MonitorHomeCompletedTaskPage extends StatefulWidget {
   const MonitorHomeCompletedTaskPage({super.key, required this.title, required this.isJoinServer});
@@ -19,49 +24,66 @@ class _MonitorHomeCompletedTaskPageState extends State<MonitorHomeCompletedTaskP
 
   @override
   Widget build(BuildContext context) {
-    return widget.isJoinServer == true
-      ? tasks.any((task) => task.status == 'complete')
-        ? ListView.builder(
+    BlocProvider.of<TaskCubit>(context).getTaskCompleted();
+
+    if (widget.isJoinServer == false) {
+      return EmError(
+        onPressed: () {
+          EmCreateGroupDialog.show(
+            context,
+            title: 'Buat Grup',
+            content: 'Tulis nama grup sesuai yang Anda inginkan',
+          );
+        },
+        textAbove: 'Anda belum mempunyai grup.',
+        textBelow: 'Buat grup terlebih dahulu!',
+        isButton: true,
+      );
+    }
+
+    return BlocBuilder<TaskCubit, DataState>(
+      builder: (context, state) {
+        List<TaskCardModel> tasks = [];
+        if (state is LoadingState) {
+          return const Center(
+              child: EmCircularLoading(size: 30, color: Color(0xffFFBD20))
+          );
+        } else if (state is SuccessState) {
+          tasks = state.data;
+        }
+
+        if (tasks.isEmpty) {
+          return EmError(
+            onPressed: () {},
+            textAbove: 'Anda belum mempunyai tugas.',
+            textBelow: 'Buat tugas terlebih dahulu!',
+          );
+        }
+
+        return ListView.builder(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           scrollDirection: Axis.vertical,
           itemCount: tasks.length,
           itemBuilder: (BuildContext context, int index) {
-            if (tasks[index].status != 'complete') return const SizedBox.shrink();
             return EmCard.task(
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const MonitorTaskDetailPage(title: 'Detail Tugas'),
+                    builder: (context) => MonitorTaskDetailPage(title: 'Detail Tugas', taskId: tasks[index].id),
                   ),
                 );
               },
               image: 'assets/images/avatar_placeholder.png',
-              title: '${tasks[index].title} yang ke-${index + 1}',
-              name: tasks[index].member.name,
-              level: tasks[index].member.level,
-              date: tasks[index].lastDate,
-              cash: tasks[index].cash,
-              experience: tasks[index].exp,
+              title: tasks[index].title,
+              name: tasks[index].member?.name ?? 'null',
+              level: '${(tasks[index].member?.level) ?? 0}' ,
+              date: DateFormatter.convertToString(tasks[index].dueDate),
+              cash: '${tasks[index].reward}',
+              experience: '${tasks[index].experience}',
             );
           },
-        )
-        : EmError(
-          onPressed: () {},
-          textAbove: 'Anda belum mempunyai tugas.',
-          textBelow: 'Buat tugas terlebih dahulu!',
-        )
-      : EmError(
-      onPressed: () {
-        EmCreateGroupDialog.show(
-          context,
-          title: 'Buat Grup',
-          content: 'Tulis nama grup sesuai yang Anda inginkan',
         );
-      },
-      textAbove: 'Anda belum mempunyai grup.',
-      textBelow: 'Buat grup terlebih dahulu!',
-      isButton: true,
-    );
+      });
   }
 }
